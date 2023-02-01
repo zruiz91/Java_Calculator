3af5ab1fd1ab62c0eaadb19e67d272a511e859f9
Zach Ruiz <powersbooth10@gmail.com>
2023-02-01 14:32:00 -0600

    added basic calculator that adds, subtracts, multiplies, divides and finds the modulo of whole numbers(int)

---
+++ Calculator.java
@@ -0,0 +1,47 @@
+/*
+This is a basic calculator that operates on whole numbers.
+*/
+
+public class Calculator{
+    public Calculator(){
+
+    }
+
+  //add method that returns an int
+    public int add(int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+  //subtract method that returns an int
+    public int subtract(int a, int b){
+        int result = a - b;
+        return result;
+    }
+
+  //multiply method that rturns an int
+    public int multiply(int a, int b){
+        int result = a * b;
+        return result;
+    }
+
+  //divide method that rturns an int
+    public int divide(int a, int b){
+        int result = a / b;
+        return result;
+    }
+
+  //modulo method that rturns an int
+    public int modulo(int a, int b){
+        int result = a % b;
+        return result;
+    }
+
+    public static void main(String[] args){
+
+        Calculator myCalculator = new Calculator();
+        System.out.println(myCalculator.add(5,7));
+        System.out.println(myCalculator.subtract(45,11));
+    }
+
+}
